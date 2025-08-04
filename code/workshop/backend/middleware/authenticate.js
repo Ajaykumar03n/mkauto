840088271ebb20990125618d45c9f5c16442e37a
Ajaykumar03n <nakofficial03@gmail.com>
2025-08-04 16:19:20 +0530

    first

--- code/workshop/backend/middleware/authenticate.js
+++ code/workshop/backend/middleware/authenticate.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET = process.env.JWT_SECRET || 'a57b2dd842d221dd543c11459831e0eb6a4ea9729320f425499d34066d3661a6b9a034d25333a0cb34aa4a3fa4f5ea3529f6a0a2fda383b9c949dd567976b0a1';
 
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
